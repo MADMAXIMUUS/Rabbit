a26c1859d62fc6c360702607fa72bda867b6bb4a
MADMAXIMUUS <maxim00nechaev@gmail.com>
2023-06-24 16:30:18 +0300

    Persian Dividers

---
+++ persian/src/main/java/ru/rabbit/persian/dividers/PersianVerticalDividers.kt
@@ -0,0 +1,66 @@
+package ru.rabbit.persian.dividers
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+object PersianVerticalDividers {
+
+    @Composable
+    fun FullHeight(
+        modifier: Modifier = Modifier,
+        sizes: DividerSizes = PersianDividerSizes.small(),
+        strokeColor: Color = MaterialTheme.colorScheme.outlineVariant
+    ) {
+        Box(
+            modifier = modifier
+                .fillMaxHeight()
+                .width(sizes.strokeThickness)
+                .background(strokeColor)
+        )
+    }
+
+    @Composable
+    fun MiddleInsets(
+        modifier: Modifier = Modifier,
+        sizes: DividerSizes = PersianDividerSizes.small(),
+        insetSide: InsetSide = InsetSide.TOP,
+        strokeColor: Color = MaterialTheme.colorScheme.outlineVariant
+    ) {
+        val padding = when (insetSide) {
+            InsetSide.TOP -> PaddingValues(top = sizes.inset)
+            InsetSide.BOTTOM -> PaddingValues(bottom = sizes.inset)
+        }
+        Box(
+            modifier = modifier
+                .padding(padding)
+                .fillMaxHeight()
+                .width(sizes.strokeThickness)
+                .background(strokeColor)
+        )
+    }
+
+    @Composable
+    fun Inset(
+        modifier: Modifier = Modifier,
+        sizes: DividerSizes = PersianDividerSizes.small(),
+        strokeColor: Color = MaterialTheme.colorScheme.outlineVariant
+    ) {
+        Box(
+            modifier = modifier
+                .padding(PaddingValues(vertical = sizes.inset))
+                .fillMaxHeight()
+                .width(sizes.strokeThickness)
+                .background(strokeColor)
+        )
+    }
+
+    enum class InsetSide { TOP, BOTTOM }
+}

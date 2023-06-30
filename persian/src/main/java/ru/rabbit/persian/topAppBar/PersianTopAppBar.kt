e50de32b7ccb4a2c35cbb4633bdf48a4dbeec426
MADMAXIMUUS <maxim00nechaev@gmail.com>
2023-06-30 19:44:34 +0300

    Update Persian TopAppBar

--- persian/src/main/java/ru/rabbit/persian/topAppBar/PersianTopAppBar.kt
+++ persian/src/main/java/ru/rabbit/persian/topAppBar/PersianTopAppBar.kt
@@ -1,21 +1,14 @@
 package ru.rabbit.persian.topAppBar
 
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import ru.rabbit.persian.foundation.PersianTheme
-import ru.rabbit.persian.foundation.icons
 
 object PersianTopAppBar {
 
@@ -24,7 +17,7 @@ object PersianTopAppBar {
     fun Primary(
         modifier: Modifier = Modifier,
         topAppBarColors: TopAppBarColors = PersianTopAppBarColors.primary(),
-        windowInsets: WindowInsets,
+        windowInsets: WindowInsets = TopAppBarDefaults.windowInsets,
         left: (@Composable PersianTopAppBarLeft.() -> Unit)? = null,
         middle: @Composable PersianTopAppBarMiddle.() -> Unit,
         right: (@Composable PersianTopAppBarRight.() -> Unit)? = null,
